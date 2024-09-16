840735a1050c383f82a21ac8435553e068b6f76e
krishkh <krishkhanna00@gmail.com>
2024-09-16 22:55:37 +0530

    made changes to git ignore and added UserModel

---
+++ src/models/UserModel.model.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+UserSchema.pre("save", async function (next) {
+  const user = this;
+
+  if (user.isModified("password")) {
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(user.password, salt);
+    user.password = hash;
+  }
+
+  next();
+});
+
+module.exports = mongoose.model("User", UserSchema);
